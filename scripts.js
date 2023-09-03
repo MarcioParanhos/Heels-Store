6028ae2c8a96ad4c4c875eedb8e135a1e0b0f897
MarcioParanhos <marcio.naga@gmail.com>
2023-09-03 17:23:05 -0300

    Adicionando produtos ao carrinho

--- scripts.js
+++ scripts.js
@@ -1,52 +1,7 @@
+// Importando os script do meu site
+import { renderCatalog } from "./src/cardProduct";
 import { initCart } from "./src/menuCart";
 
-const catalog = [
-  {
-    id: 1,
-    name: "Lingerie Cinza",
-    brand: "INSIDER",
-    price: 135.88,
-    fileImgName: "Product-1.jpeg",
-    feminine: true,
-  },
-  {
-    id: 2,
-    name: "Vestido Preto",
-    brand: "INSIDER",
-    price: 80,
-    fileImgName: "Product-2.jpeg",
-    feminine: true,
-  },
-  {
-    id: 3,
-    name: "Camisa Marrom",
-    brand: "INSIDER",
-    price: 115,
-    fileImgName: "Product-3.jpeg",
-    masculine: true,
-  },
-  {
-    id: 4,
-    name: "Camisa Preta",
-    brand: "INSIDER",
-    price: 135,
-    fileImgName: "Product-4.jpeg",
-    masculine: true,
-  },
-];
-
-//For Simplificado para mostrar todos os itens da lista (Para cada productCatalog de catalog)
-for (const productCatalog of catalog) {
-  //Monta o HTML com os dados do banco de dados
-  const productCard = `<div class='border-solid border-2 border-sky-500 w-52 my-2 m-2' id="card-produto-${productCatalog.id}">
-    <img src="./assets/img/${productCatalog.fileImgName}" alt="Produto 1 do Heels Store" style="height: 300px;"/>
-    <p>${productCatalog.brand}</p>
-    <p>${productCatalog.name}</p>
-    <p>R$ ${productCatalog.price}</p>
-    <button>Adicionar</button>
-    </div>`;
-  //Adiciona o HTML no container selecionado pelo ID
-  document.getElementById("container-produto").innerHTML += productCard;
-}
-
+// inicalizado as função  responsaveis pela funcionalidade do site
+renderCatalog()
 initCart()
--- src/menuCart.js
+++ src/menuCart.js
@@ -1,3 +1,6 @@
+import { catalog } from "../catalog";
+
+// Funcionalidades de mostrar e ocultar o carrinho
 function openCart() {
   document.getElementById("cart").classList.add("right-[0px]");
   document.getElementById("cart").classList.remove("right-[-360px]");
@@ -12,5 +15,23 @@ export function initCart() {
   const buttonCloseCart = document.getElementById("close-cart");
   const buttonOpenCart = document.getElementById("open-cart");
   buttonCloseCart.addEventListener("click", closeCart);
-  buttonOpenCart.addEventListener("click", openCart );
+  buttonOpenCart.addEventListener("click", openCart);
+}
+
+export function addToCart(idProduct) {
+  const product = catalog.find((p) => p.id === idProduct)
+  const containerProductcart = document.getElementById("products-cart");
+  const cardProductCart = `<article class="flex bg-slate-200 rounded-lg p-1 relative">
+  <button id="close-cart" class="absolute top-0 right-2">
+    <i class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i>
+  </button>
+  <img class="h-24 rounded-lg" src="./assets/img/${product.fileImgName}" alt="Carrinho: ${product.name}">
+  <div class="py-2">
+    <p class="text-slate-900 text-sm">${product.name}</p>
+    <p class="text-slate-400 text-xs">Tamanho: M</p>
+    <p class="text-green-700 text-lg">R$ ${product.price}</p>
+  </div>
+</article>`;
+
+  containerProductcart.innerHTML += cardProductCart;
 }
