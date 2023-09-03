0b044609b78d66d4add00ab8eb367b9ef97e933c
MarcioParanhos <marcio.naga@gmail.com>
2023-09-03 13:49:42 -0300

    Mandando Logo
    Signed-off-by: MarcioParanhos <marcio.naga@gmail.com>

---
+++ src/menuCart.js
@@ -0,0 +1,16 @@
+function openCart() {
+  document.getElementById("cart").classList.add("right-[0px]");
+  document.getElementById("cart").classList.remove("right-[-360px]");
+}
+
+function closeCart() {
+  document.getElementById("cart").classList.remove("right-[0px]");
+  document.getElementById("cart").classList.add("right-[-360px]");
+}
+
+export function initCart() {
+  const buttonCloseCart = document.getElementById("close-cart");
+  const buttonOpenCart = document.getElementById("open-cart");
+  buttonCloseCart.addEventListener("click", closeCart);
+  buttonOpenCart.addEventListener("click", openCart );
+}
